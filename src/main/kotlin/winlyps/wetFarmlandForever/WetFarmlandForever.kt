c65b5b62ad0e3798f7ec13e755d0b8ecdfab050f
Winlypse <nts3@o2.pl>
2025-05-29 18:31:29 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/wetFarmlandForever/WetFarmlandForever.kt
@@ -0,0 +1,14 @@
+package winlyps.wetFarmlandForever
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class WetFarmlandForever : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
